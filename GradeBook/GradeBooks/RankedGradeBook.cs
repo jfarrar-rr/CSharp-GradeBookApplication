2f244f72eff6e4bdd4dff2d80ba29a3bd005b74a
John Farrar <jfarrar@activeaero.com>
2019-07-03 12:06:20 -0400

    Converted throw to a console writeline.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,7 +34,7 @@ namespace GradeBook.GradeBooks
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades n order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades n order to properly calculate a student's overall grade.");
 
             base.CalculateStatistics();
         }
@@ -42,7 +42,7 @@ namespace GradeBook.GradeBooks
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades n order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades n order to properly calculate a student's overall grade.");
 
             base.CalculateStudentStatistics(name);
         }
